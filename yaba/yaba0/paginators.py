d72ce398d451d828b4b39266d2a831831ba6e28c
twopisoft <twopisoft@gmail.com>
2014-04-13 08:56:38 +0800

    Fix last page issue

--- yaba/yaba0/paginators.py
+++ yaba/yaba0/paginators.py
@@ -3,13 +3,13 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 class BmPaginator(Paginator):
     def validate_number(self, number):
         try:
-        	if (number=='last'):
-        		number = self.num_pages
-        	else:
-            	number = super(BmPaginator, self).validate_number(number)
+            if (number=='last'):
+                number = self.num_pages
+            else:
+                number = super(BmPaginator, self).validate_number(number)
         except InvalidPage:
             number = 1
         except EmptyPage:
-        	number = 1
+            number = 1
 
         return number
